9ff75cdf2c54572cd4883fbf2041950a0fb340e7
Flowmaster <ellyssin.gimhae@gmail.com>
2020-04-28 21:51:19 -0600

    Added blueprint-css.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -14,4 +14,19 @@
   }
 </style>
 
+<div bp="grid">
+  <div bp="4 padding-left padding-top">
+    <h3>Our Location</h3>
+    <p>
+      We are located in Boulder, Colorado. Please call to set a time to see
+      samples and to meet the team.
+    </p>
+    <p>1945 33rd Street Boulder, CO 80301</p>
+    <p>Ph: 720.366.0662</p>
+  </div>
+  <div bp="8 padding-left padding-top">
+    <h3>Reviews</h3>
+  </div>
+</div>
+
 <footer>© 2020 Apollo Ink Printing</footer>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,27 +1,30 @@
 <script>
-	import Nav from '../components/Nav.svelte';
-	import Footer from '../components/Footer.svelte';
+  import Nav from "../components/Nav.svelte";
+  import Footer from "../components/Footer.svelte";
 
-	export let segment;
+  export let segment;
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 </style>
 
-<h1>Apollo Ink Printing</h1>
-<p>Custom Screen Printing, Arts & Branding. Est-2006</p>
-<Nav {segment}/>
+<div bp="padding-left padding-top">
+  <h1>Apollo Ink Printing</h1>
+  <p>Custom Screen Printing, Arts & Branding. Est-2006</p>
+</div>
+
+<Nav {segment} />
 
 <main>
-	<slot></slot>
+  <slot />
 </main>
 
 <Footer />
