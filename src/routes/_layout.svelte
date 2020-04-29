06e03c053196c49e3f99b56820462787a8dd41e4
Flowmaster <ellyssin.gimhae@gmail.com>
2020-04-28 20:42:57 -0600

    Added svelte grid.

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,7 +1,6 @@
 <script>
 	import Nav from '../components/Nav.svelte';
 	import Footer from '../components/Footer.svelte';
-	import 'blueprint-css/dist/blueprint.min.css';
 
 	export let segment;
 </script>
--- src/routes/recent_work.svelte
+++ src/routes/recent_work.svelte
@@ -1,8 +1,68 @@
+<script>
+  import Grid from "svelte-grid";
+  import gridHelp from "svelte-grid/build/helper/index.mjs";
+  import map from "lodash.map";
+
+  const id = () =>
+    "_" +
+    Math.random()
+      .toString(36)
+      .substr(2, 9);
+
+  function generateLayout(col) {
+    return map(new Array(20), function(item, i) {
+      const y = Math.ceil(Math.random() * 4) + 1;
+      return gridHelp.item({
+        x: (i * 2) % col,
+        y: Math.floor(i / 6) * y,
+        w: 2,
+        h: y,
+        id: id()
+      });
+    });
+  }
+  const randomNumberInRange = (min, max) => Math.random() * (max - min) + min;
+  let adjustAfterRemove = false;
+  let cols = 10;
+  // Just generate messy layout
+  let layout = generateLayout(cols);
+  // Helper function which normalize. you need to pass items and columns
+  let items = gridHelp.resizeItems(layout, cols);
+  // Apply breakpoints
+  let breakpoints = [[1100, 5], [800, 4], [530, 1]];
+</script>
+
 <style>
+  .content {
+    width: 100%;
+    height: 100%;
+    color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: large;
+  }
+  /* :global(.svlt-grid-shadow) {
+    background: pink;
+  }
+  :global(.svlt-grid-container) {
+    background: #eee;
+  } */
+  .link {
+    display: flex;
+    padding-bottom: 20px;
+  }
+  .link a {
+    margin-left: 10px;
+  }
 </style>
 
 <svelte:head>
-	<title>Recent Work</title>
+  <title>Recent Work</title>
 </svelte:head>
 
-<p>recent work</p>
+<Grid {breakpoints} bind:items {cols} let:item rowHeight={100} gap={10}>
+  <div class="content" style="background: #ccc; border: 1px solid black;">
+    {item.id}
+  </div>
+</Grid>
