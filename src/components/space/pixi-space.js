6c6f92ab46d968df7a44b72231d7a8c50582f376
Flowmaster <ellyssin.gimhae@gmail.com>
2020-05-09 14:59:50 -0600

    Pixispace should have StarField.

--- src/components/space/pixi-space.js
+++ src/components/space/pixi-space.js
@@ -2,6 +2,7 @@ import * as PIXI from "pixi.js";
 import Keyboard from "pixi.js-keyboard";
 import { Ship } from "./ship.js";
 import { Hud } from "./hud.js";
+import { StarField } from "./stars.js"; 
 
 export class PixiSpace {
     constructor({ canvas: canvasElement }) {
@@ -29,52 +30,11 @@ export class PixiSpace {
             h: 30
         });
 
-        const starTexture = PIXI.Texture.from("star.png");
-        const baseSpeed = 0.025;
-        const starAmount = 1000;
-        const starBaseSize = 0.05;
-        const starStretch = 5;
-        const fov = 20;
-        let cameraZ = 0;
-        let speed = 0;
-        let warpSpeed = 0;
-
+        const starField = new StarField({app: app, count: 20000}); 
         const torpedos = [];
-
-        // init stars
-        const stars = [];
-        for (let i = 0; i < starAmount; i++) {
-            const star = {
-                sprite: new PIXI.Sprite(starTexture),
-                z: 0,
-                x: 0,
-                y: 0
-            };
-            star.sprite.anchor.x = 0.5;
-            star.sprite.anchor.y = 0.7;
-            star.sprite.alpha = 0.7;
-            star.sprite.tint = Math.random() * 0xffffff;
-            randomizeStar(star, true);
-            app.stage.addChild(star.sprite);
-            stars.push(star);
-        }
 
         //Start the game loop
         app.ticker.add(delta => loop(delta));
-
-        //  SETUP FUNCTIONS HERE
-
-        function randomizeStar(star, initial) {
-            star.z = initial
-                ? Math.random() * 2000
-                : cameraZ + Math.random() * 1000 + 2000;
-
-            // Calculate star positions with radial random coordinate so no star hits the camera.
-            const deg = Math.random() * Math.PI * 2;
-            const distance = Math.random() * 50 + 1;
-            star.x = Math.cos(deg) * distance;
-            star.y = Math.sin(deg) * distance;
-        }
 
         function input(delta) {
             // Keyboard
@@ -93,10 +53,7 @@ export class PixiSpace {
                 torpedos.push(torpedo);
             }
             if (Keyboard.isKeyDown("Space")) {
-                warpSpeed = 1;
-                setTimeout(function () {
-                    warpSpeed = 0;
-                }, 5000);
+                starField.warp();
             }
 
             Keyboard.update();
@@ -106,38 +63,7 @@ export class PixiSpace {
             input(delta);
             player.render(delta);
             hud.render(delta);
-
-            // Simple easing. This should be changed to proper easing function when used for real.
-            speed += (warpSpeed - speed) / 20;
-            cameraZ += delta * 10 * (speed + baseSpeed);
-            stars.forEach(function (star) {
-                if (star.z < cameraZ) randomizeStar(star);
-
-                // Map star 3d position to 2d with really simple projection
-                const z = star.z - cameraZ;
-                star.sprite.x =
-                    star.x * (fov / z) * app.renderer.screen.width +
-                    app.renderer.screen.width / 2;
-                star.sprite.y =
-                    star.y * (fov / z) * app.renderer.screen.width +
-                    app.renderer.screen.height / 2;
-
-                // Calculate star scale & rotation.
-                const dxCenter = star.sprite.x - app.renderer.screen.width / 2;
-                const dyCenter = star.sprite.y - app.renderer.screen.height / 2;
-                const distanceCenter = Math.sqrt(
-                    dxCenter * dxCenter + dyCenter * dyCenter
-                );
-                const distanceScale = Math.max(0, (2000 - z) / 2000);
-                star.sprite.scale.x = distanceScale * starBaseSize;
-                // Star is looking towards center so that y axis is towards center.
-                // Scale the star depending on how fast we are moving, what the stretchfactor is and depending on how far away it is from the center.
-                star.sprite.scale.y =
-                    distanceScale * starBaseSize +
-                    (distanceScale * speed * starStretch * distanceCenter) /
-                    app.renderer.screen.width;
-                star.sprite.rotation = Math.atan2(dyCenter, dxCenter) + Math.PI / 2;
-            });
+            starField.render(delta);
 
             torpedos.forEach(function (torpedo) {
                 torpedo.render();
---
+++ src/components/space/stars.js
@@ -0,0 +1,88 @@
+import * as PIXI from "pixi.js";
+
+export class StarField {
+    constructor({ app: app, count: count  }) {
+        const starTexture = PIXI.Texture.from("star.png");
+        this.app = app;
+        this.cameraZ = 0;
+        this.speed = 0;
+        this.warpSpeed = 0;
+
+        // init stars
+        const stars = [];
+        for (let i = 0; i < count; i++) {
+            const star = {
+                sprite: new PIXI.Sprite(starTexture),
+                z: 0,
+                x: 0,
+                y: 0
+            };
+            star.sprite.anchor.x = 0.5;
+            star.sprite.anchor.y = 0.7;
+            star.sprite.alpha = 0.7;
+            star.sprite.tint = Math.random() * 0xffffff;
+            this.randomizeStar(star, true);
+            app.stage.addChild(star.sprite);
+            stars.push(star);
+        }
+        this.stars = stars;
+    }
+
+    randomizeStar(star, initial) {
+        star.z = initial
+            ? Math.random() * 2000
+            : this.cameraZ + Math.random() * 1000 + 2000;
+
+        // Calculate star positions with radial random coordinate so no star hits the camera.
+        const deg = Math.random() * Math.PI * 2;
+        const distance = Math.random() * 50 + 1;
+        star.x = Math.cos(deg) * distance;
+        star.y = Math.sin(deg) * distance;
+    }
+
+    warp() {
+        this.warpSpeed = 1;
+        setTimeout(function () {
+            this.warpSpeed = 0;
+        }.bind(this), 5000);
+    }
+
+    render(delta) {
+        const baseSpeed = 0.025;
+        const starBaseSize = 0.05;
+        const starStretch = 5;
+        const fov = 20;
+        let app = this.app;
+        this.speed += (this.warpSpeed - this.speed) / 20;
+        this.cameraZ += delta * 10 * (this.speed + baseSpeed);
+
+        this.stars.forEach(function (star) {
+            if (star.z < this.cameraZ) this.randomizeStar(star);
+
+            // Map star 3d position to 2d with really simple projection
+            const z = star.z - this.cameraZ;
+            star.sprite.x =
+                star.x * (fov / z) * app.renderer.screen.width +
+                app.renderer.screen.width / 2;
+            star.sprite.y =
+                star.y * (fov / z) * app.renderer.screen.width +
+                app.renderer.screen.height / 2;
+
+            // Calculate star scale & rotation.
+            const dxCenter = star.sprite.x - app.renderer.screen.width / 2;
+            const dyCenter = star.sprite.y - app.renderer.screen.height / 2;
+            const distanceCenter = Math.sqrt(
+                dxCenter * dxCenter + dyCenter * dyCenter
+            );
+            const distanceScale = Math.max(0, (2000 - z) / 2000);
+            star.sprite.scale.x = distanceScale * starBaseSize;
+            // Star is looking towards center so that y axis is towards center.
+            // Scale the star depending on how fast we are moving, what the stretchfactor is and depending on how far away it is from the center.
+            star.sprite.scale.y =
+                distanceScale * starBaseSize +
+                (distanceScale * this.speed * starStretch * distanceCenter) /
+                app.renderer.screen.width;
+            star.sprite.rotation = Math.atan2(dyCenter, dxCenter) + Math.PI / 2;
+        }.bind(this));
+    }
+}
