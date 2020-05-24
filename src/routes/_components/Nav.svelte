4da1ed26e8a899cffb98480a0aba4814bfa0db5a
Flowmaster <ellyssin.gimhae@gmail.com>
2020-05-24 12:34:38 -0600

    Signin and signup link on nav when logged out.

--- src/routes/_components/Nav.svelte
+++ src/routes/_components/Nav.svelte
@@ -1,6 +1,9 @@
 <script>
   import IconButton, { Icon } from "@smui/icon-button";
-  import { goto } from "@sapper/app";
+  import { goto, stores } from "@sapper/app";
+
+  const { session } = stores();
+
   export let segment = null;
 </script>
 
@@ -55,36 +58,55 @@
 </style>
 
 <nav class={segment} bp="grid 1">
-  <div bp="4">
-    <div bp="grid 6">
-      <div on:click={() => goto('/space')}>joos</div>
-    </div>
-  </div>
-  <div bp="8">
-    <div bp="grid 1 gap-none">
-      <div bp="1@sm 2@md offset-10@sm offset-7@md offset-7@lg" class="outline available" on:click={() => goto('/balance')}>
-        <img class="btc-symbol" alt="" src="btc_symbol.png" />
-        <span class="balance">10.1983230</span>
-      </div>
-      <div bp="1@sm 2@md" class="outline play" on:click={() => goto('/playbook')}>
-        <img class="btc-symbol" alt="" src="btc_symbol.png" />
-        <span class="balance">0.00031233</span>
-      </div>
-      <div bp="show@xs show@sm hide@md hide@lg"> 
-        <IconButton class="material-icons" on:click={() => goto('/messages')}>
-          fastfood
-        </IconButton>
-      </div>
-      <div bp="show@md show@lg hide@sm">
-        <IconButton class="material-icons" on:click={() => goto('/messages')}>
-          message
-        </IconButton>
-      </div>
-      <div bp="show@md show@lg hide@sm">
-        <IconButton class="material-icons" on:click={() => goto('/profile')}>
-          person
-        </IconButton>
+  {#if $session.user}
+    <div bp="4">
+      <div bp="grid 6">
+        <div on:click={() => goto('/space')}>joos</div>
       </div>
     </div>
-  </div>
+    <div bp="8">
+      <div bp="grid 1 gap-none">
+        <div
+          bp="1@sm 2@md offset-10@sm offset-7@md offset-7@lg"
+          class="outline available"
+          on:click={() => goto('/balance')}>
+          <img class="btc-symbol" alt="" src="btc_symbol.png" />
+          <span class="balance">10.1983230</span>
+        </div>
+        <div
+          bp="1@sm 2@md"
+          class="outline play"
+          on:click={() => goto('/playbook')}>
+          <img class="btc-symbol" alt="" src="btc_symbol.png" />
+          <span class="balance">0.00031233</span>
+        </div>
+        <div bp="show@xs show@sm hide@md hide@lg">
+          <IconButton class="material-icons" on:click={() => goto('/messages')}>
+            fastfood
+          </IconButton>
+        </div>
+        <div bp="show@md show@lg hide@sm">
+          <IconButton class="material-icons" on:click={() => goto('/messages')}>
+            message
+          </IconButton>
+        </div>
+        <div bp="show@md show@lg hide@sm">
+          <IconButton class="material-icons" on:click={() => goto('/profile')}>
+            person
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  {:else}
+    <div bp="offset-11">
+      <div>
+        <div on:click={() => goto('/signin')}>sign in</div>
+      </div>
+    </div>
+    <div bp="offset-12">
+      <div>
+        <div on:click={() => goto('/signup')}>sign up</div>
+      </div>
+    </div>
+  {/if}
 </nav>
