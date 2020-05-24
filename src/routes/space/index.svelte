614e46ca333ebf2f91254c8eda38e8d979cbf2f6
Flowmaster <ellyssin.gimhae@gmail.com>
2020-05-23 23:27:37 -0600

    Remove old jwt store reference space.svelte.

--- src/routes/space/index.svelte
+++ src/routes/space/index.svelte
@@ -1,16 +1,13 @@
 <script>
   import { onMount } from "svelte";
-  import { jwt } from "../../stores/stores.js";
-
 
   let pixiComponent;
   //  pixi.js is a client side library. We only load this onMount
   // i.e. when the client browser loads this page 
   onMount(async () => {
-    const module = await import ('./_components/index.svelte');
+    const module = await import ('./_components/space.svelte');
 	  pixiComponent = module.default;
   });
 </script>
 
-<p>The jwt is {$jwt} </p>
 <svelte:component this={pixiComponent}/>
